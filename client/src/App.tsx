388cc797807fcd1446a0a12f867bca0d58413ad7
Your Name <r.7xell@gmail.com>
2024-04-10 17:09:09 +0100

    yeesou

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
+import './style/main.css'
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
 import Map from './components/Map';
 import About from './components/About';
-
+import Pricing from './components/Pricing';
 function App() {
   return (
     <Router>
@@ -16,11 +16,12 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/map" element={<Map />} />
           <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
         </Routes>
         <Footer />
       </div>
     </Router>
-  );
+  )
 }
 
 export default App;
