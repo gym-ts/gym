e10bf86f56b1c24387725cd360aeaf9de1642efb
Your Name <r.7xell@gmail.com>
2024-04-10 14:31:57 +0100

    yeesou

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+function App() {
+  return (
+    <div>
+      <Navbar />
+     <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/NavBar.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Navbar } from "flowbite-react";
+import { FacebookFilled,TwitterOutlined,PinterestOutlined  } from "@ant-design/icons";
+const CustomNavbar = () => {
+    return (
+        <Navbar fluid rounded>
+            <Navbar.Brand to="https://flowbite-react.com">
+                <img
+                    src="https://static.vecteezy.com/system/resources/previews/008/623/899/non_2x/gym-logo-design-template-fitness-club-creative-symbols-vector.jpg"
+                 
+                    loading="lazy"
+                    className="h-12" // Increase the value to make it bigger, e.g., h-20
+
+                />
+                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white" style={{fontWeight:'bold'}}>
+                    GYM
+                </span>
+            </Navbar.Brand>
+            <Navbar.Toggle />
+            <div className="d-flex justify-content-center align-items-center">
+                <Navbar.Collapse>
+                    <Navbar.Link className="hover:underline me-4 md:me-6" style={{ color: '#9845AA',cursor:'pointer' }} to="#">
+                        Home
+                    </Navbar.Link>
+                    <Navbar.Link className="hover:underline me-4 md:me-6" style={{cursor:'pointer'}} to="#">
+                        About
+                    </Navbar.Link>
+                    <Navbar.Link className="hover:underline me-4 md:me-6" style={{cursor:'pointer'}} to="#">
+                        Services
+                    </Navbar.Link>
+                    <Navbar.Link className="hover:underline me-4 md:me-6" style={{cursor:'pointer'}} to="#">
+                        Pricing
+                    </Navbar.Link>
+                    <Navbar.Link className="hover:underline me-4 md:me-6" style={{cursor:'pointer'}} to="#">
+                        Contact
+                    </Navbar.Link>
+                </Navbar.Collapse>
+            </div>
+            <div style={{ display: "flex", gap: 30 ,fontSize:'18px' }}>
+                <FacebookFilled style={{color:'#4267B2'}} />
+                <TwitterOutlined style={{color:'#1DA1F2'}} />
+                <PinterestOutlined style={{color:'#E60023'}} />
+            </div>
+        </Navbar>
+    );
+};
+
+export default CustomNavbar; // Rename the export to avoid conflict with imported Navbar
